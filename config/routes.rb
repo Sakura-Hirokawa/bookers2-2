3633c3984743fa4ee8fb3182eff0344e1c44cbaa
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-29 00:23:24 +0000

    [Add]新規投稿・保存・一覧画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   devise_for :users
   root to: "homes#top"
+  resources :books, only: [:create, :index , :show, :edit ,:update, :destroy]
 end
